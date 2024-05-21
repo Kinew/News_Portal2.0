7feb36d002513018050251a590ffbae31acf110f
Kirill Tarasevich <capelin@mail.ru.ru>
2024-05-22 00:30:51 +0300

    Update

--- NewsPortal/NewsPortal/news_portal/urls.py
+++ NewsPortal/NewsPortal/news_portal/urls.py
@@ -7,8 +7,6 @@ urlpatterns = [
     path('news/', cache_page(60)(views.NewsList.as_view())),
     path('<int:pk>', cache_page(300)(views.News.as_view()), name ='post_detail'),
     path('news/create/', cache_page(300)(views.PostCreate.as_view()), name='news_create'),
-    path('<int:pk>/update/', views.PostUpdate.as_view(), name='post_update'),
-    path('<int:pk>/delete/', views.PostDelete.as_view(), name='post_delete'),
     path('articles/create/', views.PostCreate.as_view(), name='articles_create'),
     path('<int:pk>/edit/', views.PostUpdate.as_view(), name='post_update'),
     path('<int:pk>/delete/', views.PostDelete.as_view(), name='post_delete'),
